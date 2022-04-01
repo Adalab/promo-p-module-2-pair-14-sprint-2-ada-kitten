5f54273319fb098775755ecfb912ce47a42ec4bb
Analía Río <analia.rio@gmail.com>
2022-04-01 11:33:32 +0200

    Arrays & loops

--- js/main.js
+++ js/main.js
@@ -10,6 +10,7 @@ const buttonCancelForm = document.querySelector('.js-btn-cancel');
 const inputDesc = document.querySelector('.js-input-desc');
 const inputPhoto = document.querySelector('.js-input-photo');
 const inputName = document.querySelector('.js-input-name');
+const inputRace = document.querySelector('.js-input-race');
 const linkNewFormElememt = document.querySelector('.js-button-new-form');
 const labelMesageError = document.querySelector('.js-label-error');
 const input_search_desc = document.querySelector('.js_in_search_desc');
@@ -79,20 +80,38 @@ function handleClickNewCatForm(event) {
         hideNewCatForm();
     }
 }
+
 //Adicionar nuevo gatito
 function addNewKitten(event) {
     event.preventDefault();
     const valueDesc = inputDesc.value;
     const valuePhoto = inputPhoto.value;
     const valueName = inputName.value;
+    const valueRace = inputRace.value;
+    const newKittenDataObject = {
+        name: valueName,
+        desc: valueDesc,
+        photo: valuePhoto,
+        race: valueRace,
+    };
+
+    const kittenDataList = [];
+    
     if (valueDesc === "" && valuePhoto === "" && valueName === "") {
         labelMesageError.innerHTML = "Debe rellenar todos los valores";
     } else {
         if (valueDesc !== "" && valuePhoto !== "" && valueName !== "") {
-            labelMesageError.innerHTML = "";
+            labelMesageError.innerHTML = "Mola! Un nuevo gatito en Adalab!";
+            inputName.value = "";
+            inputPhoto.value = "";
+            inputDesc.value = "";
         }
     }
+
+    kittenDataList.push(newKittenDataObject);
+    console.log(kittenDataList);
 }
+
 //Cancelar la búsqueda de un gatito
 function cancelNewKitten(event) {
     event.preventDefault();
@@ -114,7 +133,7 @@ function filterKitten(event) {
     }
 }
 
-//Mostrar el litado de gatitos en ell HTML
+//Mostrar el litado de gatitos en el HTML
 renderKittenList(kittenDataList);
 
 //Eventos
@@ -124,7 +143,16 @@ buttonAdd.addEventListener("click", addNewKitten);
 buttonCancelForm.addEventListener("click", cancelNewKitten);
 
 
+// Agregar un nuevo gatito al listado (lección 9 - métodos arrays)
 
+
+
+// Filtrar litado de gatitos (lección 10.2 - Filter)
+function filterKitten(ev) {
+    //Modifica el código:
+    //Haz un filter sobre el listado de gatitos
+    //Vuelve a pintar el listado de gatitos filtrados en el HTML.
+  }
 
 
 
